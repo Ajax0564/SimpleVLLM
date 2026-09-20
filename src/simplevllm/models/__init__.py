af200270db2d53621b95de3c568dbf1d18160402
Ajax0564 <ankit51200iit@gmail.com>
2026-09-20 21:22:18 +0530

    bug fix

--- src/simplevllm/models/__init__.py
+++ src/simplevllm/models/__init__.py
@@ -1,5 +1,5 @@
 from .config import QWEN3_CONFIG
 from .qwen3 import Qwen3Model, get_qwen3_model
-from .tokenizer import get_tokenizer
+from .tokenizer import get_qwen3_tokenizer
 
-__all__ = ["Qwen3Model", "get_qwen3_model", "get_tokenizer", "QWEN3_CONFIG"]
+__all__ = ["Qwen3Model", "get_qwen3_model", "get_qwen3_tokenizer", "QWEN3_CONFIG"]
